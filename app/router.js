6f8ad127669496af5df5af1f0415de6ce1176089
Nick Pachulski <nick@pachulski.me>
2015-08-18 13:32:25 -0400

    Pad inside of hashes with spaces. Do not pad inside of double curlies.
    Padding the interior of hash’s curly braces is convention. Apparently it is not convention for handlebars.

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('teams', function() {
-    this.route('show', {path: ':id'})
+    this.route('show', { path: ':id' })
   });
 });
 
